87d9bf68861978706fed8dac78838085ca522521
Yulia Kozak <ykozak74@gmail.com>
2026-06-03 21:27:45 +0300

    add base

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import Loader from "../Loader/Loader";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import SearchBar from "../SearchBar/SearchBar";
+import "./App.module.css";
+import "modern-normalize";
+//import axios from 'axios';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
